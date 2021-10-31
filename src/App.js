95ef10dad32e0c997200bdff372af7287fc091fa
Priyanshu Dangi <priyanshudangi37@gmail.com>
2021-10-31 23:35:12 +0530

    feat: add boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Mint from './components/mint/Mint.jsx';
+import Navbar from './components/navbar/Navbar.jsx';
+import Transfer from './components/transfer/Transfer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact component={Mint} path="/" />
+          <Route exact component={Transfer} path="/transfer" />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const NAME = 'Tutorial'; // the name of the app
+export const CONTRACT_ADDRESS = 'KT1VUvsQdZdBeRbnFdhfWJTi8tXSCAs5Yexi';
+export const RPC_URL = 'https://granadanet.smartpy.io'; // network rpc url
+export const NETWORK = 'granadanet';
